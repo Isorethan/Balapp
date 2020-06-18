bcedc73dcc4f688f389a3e84a297d50138037a7b
Isorethan <dessout.yannick@gmail.com>
2020-06-18 23:39:23 +0200

    modification to test neltify

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './MainRouter.css'
-import Home from '../views/home/Home';
 import Map from '../components/map/Map.js';
 
 export default function MainRouter() {
@@ -11,12 +10,7 @@ export default function MainRouter() {
             <Route exact path='/'>
                 <Map/>
             </Route>
-            <Route exact path='/'>
-
-            </Route>
-            <Route exact path='/'>
-
-            </Route>
+       
             </Switch>
         </main>
     )
