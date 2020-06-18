5c9b6dcb9d54d21435761dad8836da97435febc2
Isorethan <dessout.yannick@gmail.com>
2020-06-18 23:40:12 +0200

    nav quick modif tests

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -7,9 +7,7 @@ export default function NavBar() {
         <nav role='navigation' className='navbar'>
             <ul>
                 <li><NavLink to="/" className='navbar-links'><label htmlFor='navbar-checkbox' className='link-label' name=''>test</label></NavLink></li>
-                <li><NavLink to="/map/" className='navbar-links'>Map</NavLink></li>
-                <li><NavLink to="/" className='navbar-links'>links 3</NavLink></li>
-                <li><NavLink to="/" className='navbar-links'>links 4</NavLink></li>
+           
             </ul>
      
         </nav >
